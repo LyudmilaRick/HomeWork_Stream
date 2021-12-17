8d88eafeeb696df77b13b2820b0930c3dd51eaec
RickLyudmila <Lrick@yandex.ru>
2021-12-17 21:01:17 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course28/homework_stream/HomeWorkStreamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course28.homework_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course28/homework_stream/HomeWorkStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course28.homework_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
